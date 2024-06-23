337fca4e4c138f9870eedc58c8c0ea2c28fddb73
Farhan <o3190474834@gmail.com>
2024-06-23 09:50:06 +0500

    auth sacreen done auth part done

--- src/components/home/auth-screen/AuthButtons.tsx
+++ src/components/home/auth-screen/AuthButtons.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+"use client";
+import { Button } from "@/components/ui/button";
+import { RegisterLink, LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { useState } from "react";
 
 const AuthButtons = () => {
-  return (
-    <div>AuthButtons</div>
-  )
-}
+  const [loading, setLoading] = useState(false);
 
-export default AuthButtons
+  return (
+    <div className='flex gap-3 flex-1 md:flex-row flex-col'>
+      <RegisterLink className='flex-1' onClick={() => setLoading(true)}>
+        <Button className='w-full' variant={"outline"} disabled={loading}>
+          Sign up
+        </Button>
+      </RegisterLink>
+      <LoginLink className='flex-1' onClick={() => setLoading(true)}>
+        <Button className='w-full' disabled={loading}>
+          Login
+        </Button>
+      </LoginLink>
+    </div>
+  );
+};
+export default AuthButtons;
